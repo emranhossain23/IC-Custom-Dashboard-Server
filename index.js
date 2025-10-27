be37e5318bea7884140b3fb2c307fc2619980f42
R0F7 <wwwrafikhan075@gmail.com>
2025-10-27 18:52:27 +0600

    add user update operation

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const nodemailer = require("nodemailer");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 
+const axios = require("axios");
+
 const corsOptions = {
   origin: ["http://localhost:5173", "http://localhost:5174"],
   credentials: true,
@@ -47,9 +49,19 @@ async function run() {
 
     const db = client.db("DentalImplant");
     const usersCollection = db.collection("users");
+    const rolesCollection = db.collection("roles");
+
+    // -------- user -------
 
     // user
     app.get("/users", async (req, res) => {
+      //   const { searchText } = req.query;
+      //   const regex = new RegExp(searchText, "i");
+
+      //   const query = {
+      //     $or: [{ name: regex }, { email: regex }, { role: regex }],
+      //   };
+
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
@@ -65,7 +77,7 @@ async function run() {
 
         if (!findUser) {
           const tempPassword = Math.random().toString(36).slice(-10) + "A1#";
-          
+
           const user = await admin.auth().createUser({
             email,
             password: tempPassword,
@@ -127,6 +139,20 @@ async function run() {
       await transporter.sendMail(mailOptions);
     };
 
+    // update user
+    app.patch("/update-user", async (req, res) => {
+      const { email, selectedClients } = req.body;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: {
+          selectedClients: selectedClients,
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     // delete user
     app.delete("/delete-user/:id", async (req, res) => {
       const { id } = req.params;
@@ -134,6 +160,67 @@ async function run() {
       const result = await usersCollection.deleteOne(filter);
       res.send(result);
     });
+
+    // remove user client
+    app.delete("/remove-client", async (req, res) => {
+      const { id, user_id } = req.body;
+
+      const result = await usersCollection.updateOne(
+        { _id: new ObjectId(user_id) },
+        { $pull: { selectedClients: { id: id } } }
+      );
+      res.send(result);
+    });
+
+    // roles
+    app.get("/roles", async (req, res) => {
+      const result = await rolesCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.patch("/create-role", async (req, res) => {
+      const info = req.body;
+      const { id } = req.query;
+
+      const query =
+        id && id !== "undefined"
+          ? { _id: new ObjectId(id) }
+          : { name: info.name };
+
+      delete info?._id;
+
+      const doc = { $set: { ...info, createdAt: new Date() } };
+      const option = { upsert: true };
+
+      const result = await rolesCollection.updateOne(query, doc, option);
+      res.send(result);
+    });
+
+    // delete role
+    app.delete("/delete-role/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await rolesCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+    // let config = {
+    //   method: "get",
+    //   maxBodyLength: Infinity,
+    //   url: "https://services.leadconnectorhq.com/opportunities/search",
+    //   headers: {
+    //     Accept: "application/json",
+    //     Authorization: "pit-952e1d8e-3016-4eaf-a45a-f4bda3a2b7cc",
+    //   },
+    // };
+
+    // axios
+    //   .request(config)
+    //   .then((response) => {
+    //     console.log(JSON.stringify(response.data));
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
