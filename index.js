3aedf4832785becdf67280a8711aa912edaa7c8c
Emran Hossain <emranhossain23197@gmail.com>
2026-02-02 08:33:49 -0700

    port change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
-const port = process.env.PORT || 5100;
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const admin = require("firebase-admin");
 const nodemailer = require("nodemailer");
